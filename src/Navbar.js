049d16ef39cbe90258a2f89410d2daf70697f23f
Subu-07 <121312086+Subu-07@users.noreply.github.com>
2024-02-25 14:48:48 +0530

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>BBlog</h1>
+      <div className="links">
+        <a href="/home">Home</a>
+        <a href="/create">New Blog</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
